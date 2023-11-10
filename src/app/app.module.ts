0b768a3d8dfd92c6145fd335aa37f38d534c99fa
Luciano Soto <lusoto456@gmail.com>
2023-11-10 10:30:15 -0300

    Nav and basic css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PlayerListComponent } from './player-list/player-list.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PlayerListComponent
+    PlayerListComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
